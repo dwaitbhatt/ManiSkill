94ffc02efe4296fbdeda9fc9e8aca5f717b4eb28
Dwait Bhatt <dhbhatt@ucsd.edu>
2025-03-12 23:12:17 -0700

    Add PlaceCube task with support for panda and xarm

--- mani_skill/agents/robots/xarm6/xarm6_robotiq.py
+++ mani_skill/agents/robots/xarm6/xarm6_robotiq.py
@@ -102,6 +102,8 @@ class XArm6Robotiq(BaseAgent):
     gripper_friction = 1
     ee_link_name = "eef"
 
+    active_gripper_joint_inds = [6, 8]
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -419,6 +421,19 @@ class XArm6Robotiq(BaseAgent):
         T[:3, :3] = np.stack([ortho, closing, approaching], axis=1)
         T[:3, 3] = center
         return sapien.Pose(T)
+        
+    def get_gripper_width(self):
+        """
+        Get the width of the gripper from 0 to 1
+        0 is fully closed, 1 is fully open
+        """
+        gripper_qpos_min = self.robot.get_qlimits()[0, self.active_gripper_joint_inds[0], 0]
+        gripper_qpos_max = self.robot.get_qlimits()[0, self.active_gripper_joint_inds[0], 1]
+        max_gripper_width = gripper_qpos_max - gripper_qpos_min
+        gripper_qpos = torch.mean(self.robot.get_qpos()[:, self.active_gripper_joint_inds], axis=1)
+        # For robotiq, the gripper is closed at max qpos and open at min qpos
+        gripper_width_inv = (gripper_qpos - gripper_qpos_min) / max_gripper_width
+        return 1 - gripper_width_inv
 
     def is_static(self, threshold: float = 0.2):
         qvel = self.robot.get_qvel()[..., :-6]
